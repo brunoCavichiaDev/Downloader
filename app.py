a23d1109c031e06c8f75644b02241ccc8cb3c066
Bruno <cavichiabruno@gmail.com>
2025-03-14 14:10:35 +0100

    fix2

--- app.py
+++ app.py
@@ -61,4 +61,5 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Usa el puerto de Render o 5000 por defecto
+    app.run(host='0.0.0.0', port=port, debug=False)  # debug=False para producción
